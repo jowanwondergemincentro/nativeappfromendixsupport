4d9bea7cc3ef274f19a8d8fca3add13909712f24
jowanwondergemincentro <jowan.wondergem@incentro.com>
2021-02-25 20:39:20 +0100

    commented out mainapplication package details

--- android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
+++ android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.mendix.mendixnative.MendixReactApplication;
 import com.mendix.mendixnative.react.splash.MendixSplashScreenPresenter;
 import com.microsoft.codepush.react.CodePush;
-import com.spyworldxp.barcodescanner.BarcodeScannerPackage;
+//import com.spyworldxp.barcodescanner.BarcodeScannerPackage;
 import org.devio.rn.splashscreen.SplashScreen;
 
 import java.util.List;
@@ -27,7 +27,7 @@ public class MainApplication extends MendixReactApplication {
 
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
-        packages.add(new BarcodeScannerPackage());
+        //packages.add(new BarcodeScannerPackage());
         packages.add(new CodePush(getCodePushKey(), getApplicationContext(), BuildConfig.DEBUG));
 
         return packages;
